f625e867e64ce0637c0d6a5727eaf30f23cc3f1a
Aarsh Patil <patilaarsh@gmail.com>
2023-07-30 19:58:59 -0700

    Remove Secret Component
    Signed-off-by: Aarsh Patil <patilaarsh@gmail.com>

--- src/main.js
+++ src/main.js
@@ -6,15 +6,13 @@ import Faqs from "./components/Faqs.vue";
 import About from "./components/About.vue";
 import Projects from "./components/Projects.vue";
 import Contact from "./components/Contact.vue";
-import Secret from "./components/Secret.vue";
 
 const routes = [
     { path: '/', component: Home },
     { path: '/about', component: About },
     { path: '/projects', component: Projects },
     { path: '/contact', component: Contact },
-    { path: '/faqs', component: Faqs },
-    { path: '/secret', component: Secret },
+    { path: '/faqs', component: Faqs }
 ]
 
 const router = createRouter({
